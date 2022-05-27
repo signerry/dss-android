364621fd05ed510b233c1fc1e9636982460b6db8
egidijus.kuzma <egidijus.kuzma@gmail.com>
2022-05-27 09:47:56 +0300

    Renaming directory "test" -> "androidTest" (instrumented)

--- dss-pades-pdfbox/src/main/java/eu/europa/esig/dss/pdf/pdfbox/visible/nativedrawer/PdfBoxFontMapper.java
+++ dss-pades-pdfbox/src/main/java/eu/europa/esig/dss/pdf/pdfbox/visible/nativedrawer/PdfBoxFontMapper.java
@@ -42,50 +42,7 @@ public class PdfBoxFontMapper {
 	 * @return {@link PDType1Font}
 	 */
 	public static PDType1Font getPDFont(Font javaFont) {
-		switch (javaFont.getFamily()) {
-			case Font.SERIF:
-				if (javaFont.isPlain()) {
-					return PDType1Font.TIMES_ROMAN;
-				} else if (javaFont.isBold()) {
-					if (javaFont.isItalic()) {
-						return PDType1Font.TIMES_BOLD_ITALIC;
-					} else {
-						return PDType1Font.TIMES_BOLD;
-					}
-				} else {
-					return PDType1Font.TIMES_ITALIC;
-				}
-			case Font.SANS_SERIF:
-				if (javaFont.isPlain()) {
-					return PDType1Font.HELVETICA;
-				} else if (javaFont.isBold()) {
-					if (javaFont.isItalic()) {
-						return PDType1Font.HELVETICA_BOLD_OBLIQUE;
-					} else {
-						return PDType1Font.HELVETICA_BOLD;
-					}
-				} else {
-					return PDType1Font.HELVETICA_OBLIQUE;
-				}
-			case Font.MONOSPACED:
-				if (javaFont.isPlain()) {
-					return PDType1Font.COURIER;
-				} else if (javaFont.isBold()) {
-					if (javaFont.isItalic()) {
-						return PDType1Font.COURIER_BOLD_OBLIQUE;
-					} else {
-						return PDType1Font.COURIER_BOLD;
-					}
-				} else {
-					return PDType1Font.COURIER_OBLIQUE;
-				}
-			case Font.DIALOG:
-			case Font.DIALOG_INPUT:
-				return PDType1Font.SYMBOL;
-			default:
-				throw new UnsupportedOperationException("The font is not supported! " +
-						"Please use DSSFileFont implementation for custom fonts.");
-		}
+		return PDType1Font.TIMES_ROMAN;
 	}
 
 }

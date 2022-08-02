b3be4e70202200471cb903cf99a53a7bf47a3dfa
egidijus-kuzma <egidijus.kuzma@gmail.com>
2022-08-02 20:36:55 +0300

    DSS-UTILS. GoogleGuavaUtilsTest implicitly override AbstractUtilTest methods

--- dss-utils-google-guava/src/androidTest/java/eu/europa/esig/dss/utils/guava/impl/GoogleGuavaUtilsTest.java
+++ dss-utils-google-guava/src/androidTest/java/eu/europa/esig/dss/utils/guava/impl/GoogleGuavaUtilsTest.java
@@ -1,27 +1,290 @@
 /**
  * DSS - Digital Signature Services
  * Copyright (C) 2015 European Commission, provided under the CEF programme
- * 
+ *
  * This file is part of the "DSS - Digital Signature Services" project.
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 package eu.europa.esig.dss.utils.guava.impl;
 
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+
 import eu.europa.esig.dss.utils.AbstractUtilsTest;
 
 public class GoogleGuavaUtilsTest extends AbstractUtilsTest {
+    @Test
+    @Override
+    public void isStringEmpty() {
+        super.isStringEmpty();
+    }
 
+    @Test
+    @Override
+    public void isStringNotEmpty() {
+        super.isStringNotEmpty();
+    }
+
+    @Test
+    @Override
+    public void areAllStringsEmpty() {
+        super.areAllStringsEmpty();
+    }
+
+    @Test
+    @Override
+    public void isAtLeastOneStringNotEmpty() {
+        super.isAtLeastOneStringNotEmpty();
+    }
+
+    @Test
+    @Override
+    public void isStringBlank() {
+        super.isStringBlank();
+    }
+
+    @Test
+    @Override
+    public void isStringNotBlank() {
+        super.isStringNotBlank();
+    }
+
+    @Test
+    @Override
+    public void areStringsEqual() {
+        super.areStringsEqual();
+    }
+
+    @Test
+    @Override
+    public void areStringsEqualIgnoreCase() {
+        super.areStringsEqualIgnoreCase();
+    }
+
+    @Test
+    @Override
+    public void isStringDigits() {
+        super.isStringDigits();
+    }
+
+    @Test
+    @Override
+    public void trim() {
+        super.trim();
+    }
+
+    @Test
+    @Override
+    public void joinStrings() {
+        super.joinStrings();
+    }
+
+    @Test
+    @Override
+    public void substringAfter() {
+        super.substringAfter();
+    }
+
+    @Test
+    @Override
+    public void endsWithIgnoreCase() {
+        super.endsWithIgnoreCase();
+    }
+
+    @Test
+    @Override
+    public void lowerCase() {
+        super.lowerCase();
+    }
+
+    @Test
+    @Override
+    public void upperCase() {
+        super.upperCase();
+    }
+
+    @Test
+    @Override
+    public void isArrayEmptyObj() {
+        super.isArrayEmptyObj();
+    }
+
+    @Test
+    @Override
+    public void isArrayNotEmptyObj() {
+        super.isArrayNotEmptyObj();
+    }
+
+    @Test
+    @Override
+    public void isArrayNotEmpty() {
+        super.isArrayNotEmpty();
+    }
+
+    @Test
+    @Override
+    public void isArrayEmpty() {
+        super.isArrayEmpty();
+    }
+
+    @Test
+    @Override
+    public void subarray() {
+        super.subarray();
+    }
+
+    @Test
+    @Override
+    public void isTrue() {
+        super.isTrue();
+    }
+
+    @Test
+    @Override
+    public void isCollectionEmpty() {
+        super.isCollectionEmpty();
+    }
+
+    @Test
+    @Override
+    public void isCollectionNotEmpty() {
+        super.isCollectionNotEmpty();
+    }
+
+    @Test
+    @Override
+    public void isMapEmpty() {
+        super.isMapEmpty();
+    }
+
+    @Test
+    @Override
+    public void isMapNotEmpty() {
+        super.isMapNotEmpty();
+    }
+
+    @Test
+    @Override
+    public void collectionSize() {
+        super.collectionSize();
+    }
+
+    @Test
+    @Override
+    public void isHexEncodedTest() {
+        super.isHexEncodedTest();
+    }
+
+    @Test
+    @Override
+    public void toHexNull() {
+        super.toHexNull();
+    }
+
+    @Test
+    @Override
+    public void toHex() {
+        super.toHex();
+    }
+
+    @Test
+    @Override
+    public void fromHex() {
+        super.fromHex();
+    }
+
+    @Test
+    @Override
+    public void fromHexNull() {
+        super.fromHexNull();
+    }
+
+    @Test
+    @Override
+    public void isBase64Encoded() {
+        super.isBase64Encoded();
+    }
+
+    @Test
+    @Override
+    public void isBase64EncodedNullPointer() {
+        super.isBase64EncodedNullPointer();
+    }
+
+    @Test
+    @Override
+    public void toBase64() {
+        super.toBase64();
+    }
+
+    @Test
+    @Override
+    public void fromBase64() {
+        super.fromBase64();
+    }
+
+    @Test
+    @Override
+    public void toByteArray() throws UnsupportedEncodingException, IOException {
+        super.toByteArray();
+    }
+
+    @Test
+    @Override
+    public void toByteArrayCRLForLF() throws IOException, NoSuchAlgorithmException {
+        super.toByteArrayCRLForLF();
+    }
+
+    @Test
+    @Override
+    public void closeQuietly() throws IOException {
+        super.closeQuietly();
+    }
+
+    @Test
+    @Override
+    public void getInputStreamSize() throws IOException {
+        super.getInputStreamSize();
+    }
+
+    @Test
+    @Override
+    public void clearDirectory() throws IOException {
+        super.clearDirectory();
+    }
+
+    @Test
+    @Override
+    public void clearDirectoryNotFound() throws FileNotFoundException {
+        super.clearDirectoryNotFound();
+    }
+
+    @Test
+    @Override
+    public void reverseListTest() {
+        super.reverseListTest();
+    }
+
+    @Test
+    @Override
+    public void containsAnyTest() {
+        super.containsAnyTest();
+    }
 }

8fdb6a3a5cbae1aaed0a2a6d57c833b1f42d5a3c
egidijus-kuzma <egidijus.kuzma@gmail.com>
2022-09-03 13:42:07 +0300

    DSS-ANDROID. Adding AndroidFileCacheDataLoader

--- dss-service/src/main/java/eu/europa/esig/dss/service/http/commons/AndroidFileCacheDataLoader.java
+++ dss-service/src/main/java/eu/europa/esig/dss/service/http/commons/AndroidFileCacheDataLoader.java
@@ -18,7 +18,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-package com.signerry.android;
+package eu.europa.esig.dss.service.http.commons;
 
 import android.content.Context;
 

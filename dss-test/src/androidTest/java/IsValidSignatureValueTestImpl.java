6c88f6ad4e47239721d180c5de2ecc657b0b7e4f
egidijus-kuzma <egidijus.kuzma@gmail.com>
2022-09-22 12:53:21 +0300

    DSS-TEST. Fixing IsValidSignatureValueTest

---
+++ dss-test/src/androidTest/java/IsValidSignatureValueTestImpl.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import eu.europa.esig.dss.test.signature.IsValidSignatureValueTest;
+
+public class IsValidSignatureValueTestImpl extends IsValidSignatureValueTest {
+    @Override
+    @Test
+    public void isValidSignatureValue() {
+        super.isValidSignatureValue();
+    }
+}

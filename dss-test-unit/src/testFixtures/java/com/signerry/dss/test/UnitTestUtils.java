69c6bef9b44926121e362682a38827feb8f8292e
egidijus-kuzma <egidijus.kuzma@gmail.com>
2022-09-27 17:21:12 +0300

    DSS-TEST-UNIT. getResourceAsFile() utf8

--- dss-test-unit/src/testFixtures/java/com/signerry/dss/test/UnitTestUtils.java
+++ dss-test-unit/src/testFixtures/java/com/signerry/dss/test/UnitTestUtils.java
@@ -6,6 +6,8 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -62,7 +64,13 @@ public class UnitTestUtils implements ITestUtils {
 
     @Override
     public File getResourceAsFile(String resourcePath) {
-        return new File(UnitTestUtils.class.getResource("/" + resourcePath).getFile());
+        URL resource = UnitTestUtils.class.getResource("/" + resourcePath);
+
+        try {
+            return Paths.get(resource.toURI()).toFile();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override

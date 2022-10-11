e7fc348ebf6a1f7aa522a9e70d9c8b75178dd281
egidijus-kuzma <egidijus.kuzma@gmail.com>
2022-10-11 18:11:27 +0300

    DSS-PADES. Adding junit test listener to set android context to pdfbox

---
+++ dss-pades-pdfbox/src/test/java/eu/europa/esig/dss/UnitTestRunnerListener.java
@@ -0,0 +1,17 @@
+package eu.europa.esig.dss;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.tom_roush.pdfbox.android.PDFBoxResourceLoader;
+
+import org.junit.runner.Description;
+import org.junit.runner.notification.RunListener;
+
+public class UnitTestRunnerListener extends RunListener {
+    public void testRunStarted(Description description) throws Exception {
+           Context targetContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+       PDFBoxResourceLoader.init(targetContext);
+    }
+}

9978a1a311badfbe18233b6596054cc82985fc09
egidijus-kuzma <egidijus.kuzma@gmail.com>
2023-03-26 18:34:56 +0300

    Disabling PDFA validation

--- dss-pades-pdfbox/src/test/java/eu/europa/esig/dss/pades/signature/PdfBoxPDFASignatureSuiteTest.java
+++ dss-pades-pdfbox/src/test/java/eu/europa/esig/dss/pades/signature/PdfBoxPDFASignatureSuiteTest.java
@@ -20,13 +20,10 @@
  */
 package eu.europa.esig.dss.pades.signature;
 
-import eu.europa.esig.dss.pdfa.signature.suite.PDFAPAdESLevelBTest;
-import eu.europa.esig.dss.pdfa.signature.suite.PDFAWithFontSubsetTest;
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.Suite;
 
-@Suite
-@SelectClasses({ PDFAPAdESLevelBTest.class, PDFAWithFontSubsetTest.class })
-public class PdfBoxPDFASignatureSuiteTest {
 
-}
+//@Suite
+//@SelectClasses({ PDFAPAdESLevelBTest.class, PDFAWithFontSubsetTest.class })
+//public class PdfBoxPDFASignatureSuiteTest {
+//
+//}
